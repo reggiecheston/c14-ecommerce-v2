eed6effd1b0560ec163a04711b62f803fa258774
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-19 15:13:26 -0500

    adjusts packages

--- config-overrides.js
+++ config-overrides.js
@@ -10,6 +10,7 @@ module.exports = function override(config) {
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify"),
     url: require.resolve("url"),
+    path: require.resolve("path-browserify"),
   });
   config.resolve.fallback = fallback;
   config.ignoreWarnings = [/Failed to parse source map/];
