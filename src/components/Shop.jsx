12d927129f23640b7b327a35c49fa4f01c58877c
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-21 13:38:19 -0500

    changes axios call link

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -49,7 +49,7 @@ const Shop = () => {
       // Fetch data from server
       try {
         const response = await axios.get(
-          `https://humil-a8debfc8410b.herokuapp.com/api/shop${
+          `https://localhost:3001/api/shop${
             filter !== "none" ? "?filter=" + filter : ""
           }${
             sort !== "none"
