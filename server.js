033e7bb7a3c31cecf3a4cb098b9a66b751154e27
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-21 12:14:13 -0500

    modifies axios request

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 
-app.use("/", shopRouter);
+app.use("/api", shopRouter);
 
 module.exports = app;
 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -49,7 +49,7 @@ const Shop = () => {
       // Fetch data from server
       try {
         const response = await axios.get(
-          `https://humil-a8debfc8410b.herokuapp.com/shop${
+          `https://humil-a8debfc8410b.herokuapp.com/api/shop${
             filter !== "none" ? "?filter=" + filter : ""
           }${
             sort !== "none"
