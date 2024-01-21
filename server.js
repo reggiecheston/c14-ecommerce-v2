18e60c8dd4431d676e8a4164be5c7fed27c93ee7
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-21 12:19:01 -0500

    adds timeout to server

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ module.exports = app;
 
 const PORT = process.env.SERVERPORT || 3000;
 
+const Timeout = 30000;
+
+app.timeout = Timeout;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
