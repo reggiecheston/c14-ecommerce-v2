c0642c7264f351f009da25a81af00bbb01f0f16c
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-22 19:56:16 -0500

    changes port for db

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/", shopRouter);
 
 module.exports = router;
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVERPORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
