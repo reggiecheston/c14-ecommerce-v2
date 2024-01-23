0faaa3fd6c11ce296bb128068bc210ee79713734
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2024-01-22 20:12:56 -0500

    installs @babel/plugin-proposal-private-property-in-object package

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/", shopRouter);
 
 module.exports = router;
 
-const PORT = process.env.SERVERPORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
